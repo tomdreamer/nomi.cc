c1badaf7eb2b568cd10b1c76be8b1729d34a9818
Samuel <bouaroua.samuel@gmail.com>
2019-02-10 14:45:39 +0100

    layouting navbar and footer

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+	"DOMContentLoaded",
+	() => {
+		// console.log('IronGenerator JS imported successfully!');
+	},
+	false
+);
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+function setFooterStyle() {
+	var docHeight = $(window).height();
+	var footerHeight = $("footer").outerHeight();
+	var footerTop = $("footer").position().top + footerHeight;
+	if (footerTop < docHeight) {
+		$("footer").css("margin-top", docHeight - footerTop + "px");
+	} else {
+		$("footer").css("margin-top", "");
+	}
+	$("footer").removeClass("invisible");
+}
+$(document).ready(function() {
+	setFooterStyle();
+	window.onresize = setFooterStyle;
+});
