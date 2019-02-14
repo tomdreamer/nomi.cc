58545da2a1f1e6c87d396edd7da644f1df17ee8a
Samuel <bouaroua.samuel@gmail.com>
2019-02-14 23:15:52 +0100

    furniture image upload

--- routes/furniture-route.js
+++ routes/furniture-route.js
@@ -9,13 +9,12 @@ const fileUploader = require("../config/file-upload.js");
 /* Furniture Add */
 router.get("/furnitures/add", (req, res, next) => {
 	res.render("./furniture/add");
-	if(!req.user){
-    req.flash("danger", "You must be login");
-    res.redirect("/login");
-    // use return to STOP the function here if you are NOT logged-in
-    return;
-  }
-	
+	if (!req.user) {
+		req.flash("danger", "You must be login");
+		res.redirect("/login");
+		// use return to STOP the function here if you are NOT logged-in
+		return;
+	}
 });
 
 /* GET Furnitures index  */
@@ -71,93 +70,98 @@ router.get("/furnitures/category/:stringType", (req, res, next) => {
 });
 
 /* Furniture New (process) */
-router.post("/furnitures/new", fileUploader.single("pictureUpload"), (req, res, next) => {
-	const {
-		name,
-		width,
-		height,
-		depth,
-		description,
-		material,
-		type,
-		file
-	} = req.body;
-
-	let pictures;
-
-		if (req.file) {
-			pictures = req.file.secure_url;
-		}
-	const serialNumber = "037544f3-aebf-45c5-a8c9-107457712a6f";
-	// TODO add SKU generator to handle delivries
-	// https://www.npmjs.com/package/jsbarcode
-
-	// validation and error detail TODO LATER
-	// add express-validator https://stackoverflow.com/q/46906876
-	// preserve first user input on error
-
-	if (!req.user._id) {
-		req.flash(
-			"danger",
-			"Please login. If you're already logged and experciencing difficulties, please contact Nomi's team for help."
-		);
-
-		return res.redirect("/login");
-	}
-
-	if (req.user.role !== "designer" && req.user.role !== "admin") {
-		req.flash(
-			"danger",
-			"You lack the rights to do that, if experciencing difficulties, please contact Nomi's team for help."
-		);
-		return res.redirect("/login");
-	}
-
-	//const ownerOfDesign = req.user._id;
-	console.log(req.user);
-
-	Furniture.findOne({ name: name })
-		.then(queryResult => {
-			req.flash(
-				"danger",
-				`A Furniture called ${queryResult.name}  already exists.`
-			);
-			res.redirect("/dashboard");
-		})
-
-		.catch(err => next(err));
-		const creator = req.user._id;
-	const newFurniture = new Furniture({
-		name,
-		serialNumber,
-		size: {
+router.post(
+	"/furnitures/new",
+	fileUploader.single("specificationsFile"),
+	(req, res, next) => {
+		const {
+			name,
 			width,
 			height,
-			depth
-		},
-		material,
-		type,
-		description,
-		creator,
-		pictures
-	});
+			depth,
+			description,
+			material,
+			type
+		} = req.body;
 
-	newFurniture
-		.save()
-		.then(queryResult => {
+		// TODO add SKU generator to handle delivries
+		// https://www.npmjs.com/package/jsbarcode
+
+		// validation and error detail TODO LATER
+		// add express-validator https://stackoverflow.com/q/46906876
+		// preserve first user input on error
+
+		if (!req.user._id) {
 			req.flash(
-				"success",
-				"Your Design has been submitted, you will receive our feedback soon !"
+				"danger",
+				"Please login. If you're already logged and experciencing difficulties, please contact Nomi's team for help."
 			);
-			res.redirect(`/furnitures/${queryResult._id}`);
-		})
-		.catch(error => {
-			// refill user form FIXME
-			req.flash("danger", "Something went wrong..");
-			console.log(error);
-			res.redirect("/furnitures/add");
+
+			return res.redirect("/login");
+		}
+
+		if (req.user.role !== "designer" && req.user.role !== "admin") {
+			req.flash(
+				"danger",
+				"You lack the rights to do that, if experciencing difficulties, please contact Nomi's team for help."
+			);
+			return res.redirect("/login");
+		}
+
+		Furniture.findOne({ name: name })
+			.then(queryResult => {
+				req.flash(
+					"danger",
+					`A Furniture called ${queryResult.name}  already exists.`
+				);
+				return res.redirect("/dashboard");
+			})
+
+			.catch(err => next(err));
+
+		const creator = req.user._id;
+
+		let picture;
+
+		if (req.file) {
+			picture = req.file.secure_url;
+		}
+
+		const serialNumber = "037544f3-aebf-45c5-a8c9-107457712a6f";
+
+		const newFurniture = new Furniture({
+			name,
+			serialNumber,
+			size: {
+				width,
+				height,
+				depth
+			},
+			material,
+			type,
+			description,
+			creator,
+			pictures: { url: [picture] }
 		});
-});
+
+		newFurniture
+			.save()
+			.then(queryResult => {
+				req.flash(
+					"success",
+					"Your Design has been submitted, you will receive our feedback soon !"
+				);
+				res.redirect(`/furnitures/${queryResult._id}`);
+			})
+			.catch(error => {
+				// refill user form FIXME
+				req.flash("danger", "Something went wrong..");
+				console.log(error);
+
+				res.redirect("/furnitures/add");
+			});
+	}
+);
 
 /* Furniture Edit */
 router.get("/furnitures/:furnitureId/edit", (req, res, next) => {
