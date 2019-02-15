178ee634646b64c649c852d8eefebe61b9bdcfca
tomdreamer <thomas.lesage@hotmail.com>
2019-02-15 12:59:18 +0100

    Added id of creator in quote model

--- routes/user-quotes-route.js
+++ routes/user-quotes-route.js
@@ -6,7 +6,6 @@ const User = require("../models/user-model");
 const Furniture = require("../models/furniture-model");
 const Quote = require("../models/quote-model");
 
-<<<<<<< HEAD
     router.get("/collaboration", (req, res, next)=>{
         console.log(Furniture.findOne({isActive :{$eq: false} }));
 
@@ -59,56 +58,3 @@ const Quote = require("../models/quote-model");
     })
 
     module.exports = router;
-=======
-router.get("/collaboration", (req, res, next) => {
-	console.log(Furniture.findOne({ isActive: { $eq: false } }));
-
-	Furniture.find({ isActive: { $eq: false } })
-		.populate("creator")
-		.sort({ createdAt: -1 })
-		.then(queryResult => {
-			res.locals.furnitureArray = queryResult;
-			res.render("auth-views/user-crafter-concepts-list.hbs");
-		})
-
-		.catch(err => next(err));
-});
-
-router.get("/furnitures/:furnitureId/quote", (req, res, next) => {
-	const { furnitureId } = req.params;
-
-	Quote.findOne({ furniture: furnitureId, crafter: req.user._id })
-		.then(quoteDoc => {
-			res.locals.quote = quoteDoc;
-
-			Furniture.findById(furnitureId)
-				.then(queryResult => {
-					res.locals.furnitureItem = queryResult;
-					res.locals.isShopping = true;
-					res.render("auth-views/user-crafter-like.hbs");
-				})
-
-				.catch(err => next(err));
-		})
-		.catch(err => next(err));
-});
-
-router.get("/collaboration/:furnitureItemId/check", (req, res, next) => {
-	const { furnitureItemId } = req.params;
-
-	Quote.create({ furniture: furnitureItemId, crafter: req.user._id })
-
-		.catch(err => next(err))
-		.then(() => res.redirect("/collaboration"));
-});
-
-router.get("/collaboration/:quoteId/delete", (req, res, next) => {
-	const { quoteId } = req.params;
-
-	Quote.findByIdAndRemove(quoteId)
-		.catch(err => next(err))
-		.then(() => res.redirect("/collaboration"));
-});
-
-module.exports = router;
->>>>>>> a5b5186a7fb26bf211fbc849a4cf50a037103c71
